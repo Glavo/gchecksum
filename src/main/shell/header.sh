a7314bb741fe5d68028cfe0af80ab7b3d94c6b29
Glavo <zjx001202@126.com>
2021-06-19 08:46:44 +0800

    更新 header.sh

--- src/main/shell/header.sh
+++ src/main/shell/header.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-exec java -Xmx1g -XX:+UseG1GC $JAVA_OPTS $GCHECKSUM_JAVA_OPTS -cp "$0" 'org.glavo.checksum.Main' "$@"
+_java=java
+if test -n "$JAVA_HOME"; then
+    _java="$JAVA_HOME/bin/java"
+fi
+if test -n "$GCHECKSUM_JAVA_HOME"; then
+    _java="$GCHECKSUM_JAVA_HOME/bin/java"
+fi
+exec "$_java" -Xmx1g -XX:+UseG1GC $JAVA_OPTS $GCHECKSUM_JAVA_OPTS -cp "$0" 'org.glavo.checksum.Main' "$@"
 exit 1
