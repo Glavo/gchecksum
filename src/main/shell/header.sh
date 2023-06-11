afd2f9f7ce1c1b0f8782c90875ecd77a04ec82ac
Glavo <zjx001202@gmail.com>
2023-06-12 00:59:20 +0800

    update shell header

--- src/main/shell/header.sh
+++ src/main/shell/header.sh
@@ -7,9 +7,5 @@ if test -n "$GCHECKSUM_JAVA_HOME"; then
     _java="$GCHECKSUM_JAVA_HOME/bin/java"
 fi
 
-if test -n "$GCHECKSUM_JAVA_OPTS"; then
-    GCHECKSUM_JAVA_OPTS='-Xmx2g -XX:+UseG1GC'
-fi
-
 exec "$_java" $GCHECKSUM_JAVA_OPTS -cp "$0" 'org.glavo.checksum.Main' "$@" $GCHECKSUM_OPTS
 exit 1
